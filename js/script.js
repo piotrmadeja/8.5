87257423c775a4fe3554c4606f749f377f050820
Piotr Madeja <piotr.madeja19@gmail.com>
2018-02-26 18:55:07 +0100

    8.5

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var a = prompt('Base a'),
+    h = prompt('Height h'),
+    triangleArea = a*h/2;
+
+alert('Your triangle field is : ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
